cc2b4f91edb9ada83733192c6f2fca8c04c383e1
yinhuochong <350905402@qq.com>
2019-09-19 16:54:39 +0800

    fix es start fail

--- docker/iast-cloud/images/apache-php7.2/start.sh
+++ docker/iast-cloud/images/apache-php7.2/start.sh
@@ -3,11 +3,11 @@
 cp /usr/local/etc/php/php.ini-development /usr/local/etc/php/php.ini
 
 echo '[-] Waiting rasp-cloud'
-    while true
-    do
-        curl rasp-cloud:8086 &>/dev/null && break
-        sleep 1
-    done
+while true
+do
+    curl rasp-cloud:8086 &>/dev/null && break
+    sleep 1
+done
 
 /bin/bash /etc/init.d/apache2 start
 
